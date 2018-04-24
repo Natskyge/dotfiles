3a290a984705425d9728248cc63ba8f9e7e0da37
natskyge <natskyge@void.home>
2018-04-24 20:28:03 +0200

    Add init.sh, the totaly working init script!

---
+++ init.sh
@@ -0,0 +1,23 @@
+echo "Attempting to create symbolic links"
+ln -s -f ~/dotfiles/2bwm/config.h ~/pkgsrc/2bwm/config.h 
+ln -s -f ~/dotfiles/dmenu2/dmenu2_run ~/bin/dmenu2_run
+ln -s -f ~/dotfiles/dots/.Xmodmap ~/.Xmodmap 
+ln -s -f ~/dotfiles/dots/.Xresources ~/.Xresources 
+ln -s -f ~/dotfiles/dots/.bash_profile ~/.bash_profile 
+ln -s -f ~/dotfiles/dots/.bashrc ~/.bashrc 
+ln -s -f ~/dotfiles/dots/.xbindkeysrc ~/.xbindkeysrc 
+ln -s -f ~/dotfiles/dots/.xinitrc ~/.xinitrc 
+ln -s -f ~/dotfiles/lemonbar/bar ~/bin/bar 
+ln -s -f ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim 
+ln -s -f ~/dotfiles/scripts/a ~/bin/a 
+ln -s -f ~/dotfiles/scripts/cmusnp ~/bin/cmusnp 
+ln -s -f ~/dotfiles/scripts/git-status ~/bin/git-status 
+ln -s -f ~/dotfiles/scripts/playpause ~/bin/playpause 
+ln -s -f ~/dotfiles/scripts/popup ~/bin/popup 
+ln -s -f ~/dotfiles/scripts/ufetch ~/bin/ufetch 
+ln -s -f ~/dotfiles/scripts/update ~/bin/update 
+ln -s -f ~/dotfiles/scripts/volume ~/bin/volume 
+ln -s -f ~/dotfiles/st/config.h ~/pkgsrc/st/config.h 
+ln -s -f ~/dotfiles/vis/visrc.lua ~/.config/vis/visrc.lua 
+ln -s -f ~/dotfiles/zathura/zathurarc ~/.config/zathura/zathurarc 
+echo "Success!"
